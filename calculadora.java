bbb29994b44e2f54cdb35b56a7e53be96781601f
Alan Sepulveda <alrd1718@gmail.com>
2026-02-26 16:41:49 +0100

    Commit Inicial

---
+++ calculadora.java
@@ -0,0 +1,14 @@
+public class calculadora{
+
+    private int num1;
+    private int num2;
+
+    calculadora() {
+    }
+
+    public int suma(int num1, int num2){
+        
+        return num1+num2;
+    }
+
+}
---
+++ pruebaCalculadora.java
@@ -0,0 +1,11 @@
+public class pruebaCalculadora{
+
+public static void main(String[]args){
+
+    calculadora calc = new calculadora();
+
+    System.out.println(calc.suma(6, 7));
+    
+}
+
+}
